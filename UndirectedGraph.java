99d60ae581ec92087fa0c8c5f4b58383e345aee4
Ziyan Liu <z363liu@edu.uwaterloo.ca>
2019-05-22 15:14:29 -0400

    update

--- UndirectedGraph.java
+++ UndirectedGraph.java
@@ -11,7 +11,6 @@ class UndirectedGraph {
     public UndirectedGraph(){
     	nodes = new HashMap<Integer, Node>();
     	edges = new ArrayList<Edge>();
-//    	makeNodes(nodeIDList);
     }
     
     public void build(int aID, int bID){
@@ -23,103 +22,51 @@ class UndirectedGraph {
     	Edge edge = new Edge(aID, bID);
     	edges.add(edge);
     }
-    
-//    // makeSet
-//    public void makeNodes(ArrayList<Integer> nodeIDList) {
-//    	for(Integer nodeID: nodeIDList) {
-//    		Node node = new Node(nodeID);
-////    		System.out.print("Debug: " + node.id);
-//    		nodes.put(node.id, node);
-//    	
-//    	}
-//    }
-    
-    public Node findRoot(Node node) {
+
+    public int findRoot(Node node) {
     	if (node.parentID != node.id)
     		// parent node
-    		return findRoot(nodes.get(node.parentID));
-    	return node;
+    		node.parentID = findRoot(nodes.get(node.parentID));
+    	return node.parentID;
     }
     
     public void merge(Node a, Node b) {
-    	Node aRoot = findRoot(a);
-    	Node bRoot = findRoot(b);
+    	Node aRoot = nodes.get(findRoot(a));
+    	Node bRoot = nodes.get(findRoot(b));
     	
     	if (aRoot.id != bRoot.id) {
         	// if aRoot's rank > bRoot's rank
         	if (aRoot.rank > bRoot.rank) {
             	// attach bRoot to a's tree
         		bRoot.parentID = aRoot.id;
-        		nodes.get(a.id).parentID = aRoot.id;
-        		nodes.get(b.id).parentID = aRoot.id;
             	// if aRoot's rank < bRoot's rank
         	} else if (aRoot.rank < bRoot.rank) {
         		// attach aRoot to b's tree
         		aRoot.parentID = bRoot.id;
-        		nodes.get(a.id).parentID = bRoot.id;
-        		nodes.get(b.id).parentID = bRoot.id;
         		// if aRoot's rank == bRoot's rank
         	} else {
         		// choose any root nodes to be the parent
         		bRoot.parentID = aRoot.id;
-        		nodes.get(a.id).parentID = aRoot.id;
-        		nodes.get(b.id).parentID = aRoot.id;
         		// increase the rank
         		aRoot.rank ++;
         	}
-    	} else {
-    		if (a.parentID != aRoot.id) {
-    			a.parentID = aRoot.id;
-    		}
-    		if (b.parentID != aRoot.id) {
-    			b.parentID = aRoot.id;
-    		}
-    	}
+    	} 
     }
     
     public void connectEdges() {
-    	for(Edge edge : edges) {
-    		
+    	for(Edge edge : edges) {		
     		Node a = nodes.get(edge.aID);
     		Node b = nodes.get(edge.bID);
-    		
-    		Node aRoot = findRoot(a);
-    		Node bRoot = findRoot(b);
-    		
     		merge(a, b);
     	}
-//    	printNodes();
     }
     
     public String generateLabels() {
     	StringBuilder sb = new StringBuilder();
         for (HashMap.Entry<Integer, Node> entry: nodes.entrySet()) {
-        	sb.append(entry.getKey() + " " + findRoot(entry.getValue()).id + "\n");
+        	sb.append(entry.getKey() + " " + findRoot(entry.getValue()) + "\n");
         }
         return sb.toString();
-    }
-    
-    public void printNodes(){
-        //Find different Sets
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-        for (HashMap.Entry<Integer, Node> entry: nodes.entrySet()) {
-            if(map.containsKey(entry.getValue().parentID)){
-                ArrayList<Integer> list = map.get(entry.getValue().parentID);
-                list.add(entry.getKey());//add vertex
-                map.put(entry.getValue().parentID, list);
-            }else{
-                ArrayList<Integer> list = new ArrayList<>();
-                list.add(entry.getKey());
-                map.put(entry.getValue().parentID, list);
-            }
-        }
-        //Print the Different sets
-        Set<Integer> set = map.keySet();
-        Iterator<Integer> iterator = set.iterator();
-        while(iterator.hasNext()){
-            int key = iterator.next();
-            System.out.println("Set Id: " + key + " elements: " + map.get(key));
-        }
     }
     
 //    public static void main(String args[]){
